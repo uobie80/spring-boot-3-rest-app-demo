2f76cbccaf978d2d8cc48215ee56e47dfad51365
Uchenna Obicheta <uobicheta@hotmail.com>
2024-05-26 17:42:16 -0400

    Updated pom.xml,application.properties to use H2 instead of sqlite and renamed seed.sql file.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS user (
-    phone_number TEXT PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT NULL,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL,
-    roles TEXT NOT NULL
+CREATE TABLE IF NOT EXISTS users (
+    phone_number VARCHAR(15) PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100),
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    roles VARCHAR(100) NOT NULL
 );
